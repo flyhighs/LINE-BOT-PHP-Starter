f12360e90e52469ef9e36216f49b7f6487e99a4a
flyhighs <besleepy24hrs@gmaiol.com>
2016-11-08 16:11:19 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			}else if($str[0] == "bird"){
 				$res = "นกไงไอ้โง่";
 			}else{
-				$res = "กูจะรู้ไหมละไอ้สัด่";
+				$res = "กูจะรู้ไหมละไอ้สัด";
 			}
 			
 			// Build message to reply back
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $res
 			];
-
+	
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -50,9 +50,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			$result = curl_exec($ch);
 			curl_close($ch);
-
-			echo $result . "\r\n";
+			
 			echo "ความเอ้ย ถามอะไรโง่ๆ";
+			echo $result . "\r\n";
+			
 		}
 	}
 }
